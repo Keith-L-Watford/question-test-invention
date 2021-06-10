02fb50847b2123b8ed01b43061489f9d30437fcf
Keith Watford <keith.l.watford@gmail.com>
2021-06-09 22:06:47 -0400

    Wrote the README. Tried continueing with dynamic elements and somehow broke them.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# question-test-invention
+# Trivia Quiz
+
+## Description
+I was assigned to create a dynamic multiple choice quiz using primarily JavaScript. I attempted this to challenge myself and my knowledge of JS while also creating a fun little game quiz. I quickly learned that I need to continue working on my JS syntax.
+
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Troubleshooting](#Troubleshooting)
+- [License](#license)
+
+## Installation
+No installs required.
+
+## Usage
+Click the URL and the application should load in your default browser.
+
+## Credits
+n/a
+
+## Troubleshooting
+I had the start button and timer working, I also was able to append the answer buttons to the html via JS. Once both were set up, everything broke. Unfortunately, I was able to figure why that happened. I had ideas about how to implement questions and choices using an array of obects and somehow configure it to switch using some kind of For Loop function. I was created some code for utilizing the local storage to save high scores, however I didnt get far because I was trying to troubleshoot the buttons. 
+
+## License
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to http://unlicense.org/
--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,51 @@
 // Variables
 var scoreEl = document.querySelector(".current-score")
 var timerEl = document.querySelector(".current-time")
+var questionDiv = document.querySelector(".question")
+var AnswersDiv = document.querySelector(".answers")
 
-var scoreNum = 0
+
+var choiceOne = document.createElement("button");
+var choiceTwo = document.createElement("button");
+var choiceThree = document.createElement("button");
+var choiceFour = document.createElement("button");
 
 
-// the button
+// the Start button
 var btn = document.createElement("button");
 btn.textContent = "Let's Start!";
-document.body.appendChild(btn);
+document.getElementsByClassName("button").appendChild(btn);
+
+
+// The Answer
+AnswersDiv.appendChild(choiceOne)
+AnswersDiv.appendChild(choiceTwo)
+AnswersDiv.appendChild(choiceThree)
+AnswersDiv.appendChild(choiceFour)
+
+choiceOne.setAttribute("style", " color:white; background: white; width: 50px; height: 20px;");
+choiceTwo.setAttribute("style", " color:white; background: white; width: 50px; height: 20px;");
+choiceThree.setAttribute("style", " color:white; background: white; width: 50px; height: 20px;");
+choiceFour.setAttribute("style", " color:white; background: white; width: 50px; height: 20px;");
+
+var scoreNum = 0
+var questionNum = 0
+
+
+// for (let i = 0; i < quizQuestions.length; i++) {
+//     const element = quizQuestions[i];    
+// }
 
 
 // WHEN I click the start button
 // THEN a timer starts* and I am presented with a question
 // A TIMER FUNCTION
-var secondsRemaining = 12
+var secondsRemaining = 60
 
 function myTimer() {
     var timerInterval = setInterval(function () {
     
+        // btn.disabled
         if (secondsRemaining >= 2) {
             timerEl.textContent = secondsRemaining + " seconds remaining";
             secondsRemaining--;
@@ -32,6 +59,14 @@ function myTimer() {
         }
     }, 1000);
 }
+
+// function showMe() {
+//     ("style", "display: inline")
+// }
+
+// function hideMe() {
+//         btn.setAttribute("style", "display: none;")
+// }
 
 // //  The questions with choices and correct answer
 // function theQuiz(params) {    
@@ -63,16 +98,16 @@ var quizQuestions = [
     },
 ]
 
-if (quizQuestions.options === quizQuestions.correctAnswer) {
-    scoreEl++;
-} else {
-    secondsRemaining - 5;
-}
+// if (quizQuestions.options === quizQuestions.correctAnswer) {
+//     scoreEl++;
+// } else {
+//     secondsRemaining - 5;
+// }
 
 
 
-
-
+btn.addEventListener('click', showMe);
+btn.addEventListener('click', hideMe);
 btn.addEventListener('click', myTimer);
 // btn.addEventListener("click", theQuiz);
 
@@ -93,3 +128,14 @@ btn.addEventListener('click', myTimer);
 
 // WHEN the game is over
 // THEN I can save my initials and my score
+function highScore() {
+    var score = localStorage.getItem("score");
+    var initials = localStorage.getItem("intials");
+  
+  }
+
+localStorage.setItem("score", score);
+localStorage.setItem("Initials", Initals);
+
+
+
